af456a465d9223330d0436e6674ad57854f3c47e
AZHAR <azharsaepudin5@gmail.com>
2019-11-04 18:49:09 +0700

    navigation component jetpack

---
+++ app/src/main/java/com/studio/azhar/navigationandroidjetpack/AkunFragment.kt
@@ -0,0 +1,19 @@
+package com.studio.azhar.navigationandroidjetpack
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AkunFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_akun, container, false)
+    }
+
+}
