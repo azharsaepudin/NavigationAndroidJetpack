acc558d026daf54f49212aaad42edd760cb48afd
AZHAR <azharsaepudin5@gmail.com>
2019-11-12 21:26:02 +0700

    add navigation from fragment to activity

---
+++ app/src/main/java/com/studio/azhar/navigationandroidjetpack/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.studio.azhar.navigationandroidjetpack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
--- app/src/main/java/com/studio/azhar/navigationandroidjetpack/HomeFragment.kt
+++ app/src/main/java/com/studio/azhar/navigationandroidjetpack/HomeFragment.kt
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -15,6 +17,15 @@ class HomeFragment : Fragment() {
 
 
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btn_activity.setOnClickListener {
+
+            findNavController().navigate(R.id.action_destination_home_to_detailActivity)
+        }
     }
 
 }
